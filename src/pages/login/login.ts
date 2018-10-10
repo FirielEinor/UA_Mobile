200c1bb3c6d48c25ccac543470de933af90fdec5
PATRY <Firiel@wifi-eduroam0939.utt.fr>
2018-10-10 15:18:05 +0200

    add img for UA logo and change front of login page

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ModalController } from 'ionic-angular';
 import { TabsPage } from '../tabs/tabs';
+import { LoginModal } from './loginModal/loginModal';
 
 @Component({
     selector: 'page-login',
     templateUrl: 'login.html'
 })
 export class LoginPage {
-    constructor(public navCtrl: NavController) {
+    constructor(public navCtrl: NavController, public modalCtrl: ModalController) {
 
     }
 
     login() {
-        this.navCtrl.push(TabsPage)
+        //this.navCtrl.push(TabsPage)
+        let profileModal = this.modalCtrl.create(LoginModal, { userId: 8675309 });
+        profileModal.present();
     }
 }
