8db29d29b91c9004b052f74e91760a748d078939
PATRY <Firiel@wifi-eduroam0939.utt.fr>
2018-10-10 15:15:23 +0200

    add team page

---
+++ src/pages/team/team.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+    selector: 'page-team',
+    templateUrl: 'team.html'
+})
+export class TeamPage {
+    constructor(public navCtrl: NavController) {  }
+
+}
